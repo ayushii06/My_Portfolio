c238ab7985ec850cb39347912fb1b30ff987145d
ayushi pal <kaggle86@gmail.com>
2024-04-04 15:40:38 +0530

    added resume link and fixed issues

--- Pages/index.js
+++ Pages/index.js
@@ -11,19 +11,3 @@ bars.addEventListener("click",()=>{
         list[0].style.visibility="visible";
     }
 })
-
-let bg = document.getElementsByClassName("background")[0];
-bg.addEventListener('mouseover',()=>{
-    console.log("dsgr")
-})
-
-
-
-
-// for (let i = 0; i < card.length; i++) {
-//     card[i].addEventListener('mouseover',(i)=>{
-       
-//     });
-// }
-
-document.getElementById("about001").scrollIntoView();
